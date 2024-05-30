56d4782f25230e5233cdef75119c5533f038e5e7
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2024-05-30 15:45:54 +0300

    Added progress bars for attributes and a table for evolution

--- src/app/pokemon/[pokemon_id]/pokemon.tsx
+++ src/app/pokemon/[pokemon_id]/pokemon.tsx
@@ -1,6 +1,5 @@
 import Pokemon from "@/model/pokemon";
-import { Row, Col, Container, Image, ListGroup } from 'react-bootstrap';
-
+import { Row, Col, Container, Image, ListGroup, ProgressBar, Badge } from 'react-bootstrap';
 
 type Props = {
     pokemon: Pokemon;
@@ -21,23 +20,46 @@ export default function PokemonComponent(props: Props) {
                     <Image src={pokemon.mainImage} thumbnail />
                 </Col>
                 <Col>
-                    Pokémon Properties
+                    <Row>
+                        Pokémon Properties
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>
+                                <p>Attack</p>
+                                <ProgressBar variant="danger" now={pokemon.attack} label={`${pokemon.attack}`} />
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <p>Defense</p>
+                                <ProgressBar variant="info" now={pokemon.defense} label={`${pokemon.defense}`} />
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <p>Speed</p>
+                                <ProgressBar variant="warning" now={pokemon.speed} label={`${pokemon.speed}`} />
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <p>Health</p>
+                                <ProgressBar variant="success" max={300} now={pokemon.healthPoints} label={`${pokemon.healthPoints}`} />
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Row>
+                    <br />
+                    <Row>
+                        <ListGroup>
+                            <ListGroup.Item> <b>Evolation Family</b> </ListGroup.Item>
+                            <ListGroup.Item>
+                                {pokemon.devolution} <Badge bg="danger">Devolution</Badge>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {pokemon.pokemonName} <Badge bg="info">Current</Badge>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {pokemon.evolution} <Badge bg="success">Evolution</Badge>
+                            </ListGroup.Item>
+
+                        </ListGroup>
+                    </Row>
                 </Col>
             </Row>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>
-                    <p>Attack</p>
-                    {pokemon.attack}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <p>Defense</p>
-                    {pokemon.defense}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <p>Health</p>
-                    {pokemon.healthPoints}
-                </ListGroup.Item>
-            </ListGroup>
+
         </Container>
     );
 }
